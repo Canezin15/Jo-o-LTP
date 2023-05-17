67d3476827e76d2703f6a24312849b8161c5cae1
Canezin15 <jp1603207@gmail.com>
2023-05-17 14:22:33 -0400

    commit inicial

--- script.js
+++ script.js
@@ -20,6 +20,14 @@ class Produto {
 class Produto_destaque extends Produto{
     constructor(nome, dataCadastro, descricao, preco, imagemDestaque)
     super(nome, dataCadastro, descricao, preco, imagemDestaque)
+    mostrar_produto_destaque(){
+                    <div>${this.data_cadastro}</div>
+                    <div>${this.descricao}</div>
+                    <div>${this.preco}</div>
+                    <div>${this.imagemDestaque}</div>`                
+    }
 }
 
 const produto = new Produto("New Fiesta", "08/05/2023", "carro", "45000")
+const produtoDestaque = new Produto_destaque("Gran Siena", "17/05/2023", 42000, "carro.jpg")
+const produto = new Produto("New Fiesta", "08/05/2023", "carro", 35000)
